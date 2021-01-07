ff9aedceef955987f5e472f784385d69a03d4081
Snow Day <jakedoty@gmail.com>
2021-01-06 19:36:43 -0600

    Adding values

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var pwLength;
-var lowCase;
-var upCase;
-var numeric;
-var specialChar;
 
-function getInputs() {
-  prompt()
-}
+// 
+// function getInputs() {
+  var pwLength = prompt("How long would you like your password? (choose a number from 8 to 128");
+  var lowCase = confirm("Would you like it to include lowercase letters?");
+  var upCase = confirm("Uppercase letters?");
+  var numeric = confirm("Numbers?");
+  var specialChar = confirm("Special characters?");
+// }
+console.log(pwLength);
+// function genRandUp() {
+//   if (document.getElementById(upCase == true)) {
+//     return String.fromCharCode(Math.floor(Math.random()*26) + 65);
+
+//   } else {
+//       return ('');
+//   }
+// }
+// getInputs();
+// console.log(pwLength);
+// function generatePassword() {
+//   return pwlength;
+// }
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
